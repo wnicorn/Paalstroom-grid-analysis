30db3de5bc2e06cd1a18472ca160d50ef340561f
Elise <e.v.wijngaarden@student.tue.nl>
2022-05-14 16:54:35 +0200

    incl per scenario

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,92 +1,128 @@
 import pandas as pd
+import os
+import time
+
+start = time.time()
 
 # prep
-filename = 'EDITED_nederweert-sl-ndw.laurs.xlsx'
 engine = 'openpyxl'
-sheets = ['Knooppunten', 'Takken', 'Elementen', 'Hoofdkabels']
-nodes = ['UL1', 'UL2', 'UL3']
-loading = 'Belastinggraad'
-connections = ['UL1N', 'UL2N', 'UL3N']
-sort = 'Soort'
-trafo = 'transformator'
-cable = 'kabel'
-voltages = nodes + connections
-include = voltages + [loading, sort]
+language = 'English' # 'Nederlands'
+if language == 'English':
+    sheets = ['Nodes', 'Branches', 'Elements', 'Switches and protections'] #, 'Hoofdkabels']
+    loading = 'Load rate' #'Belastinggraad'
+    voltages_phase = ['UL1', 'UL2', 'UL3']
+    voltages_phase_n = ['UL1N', 'UL2N', 'UL3N']
+    sort = 'Sort'
+    trafo = 'transformer'
+    cable = 'cable'
+elif language == 'Nederlands':
+    sheets = ['Knooppunten', 'Takken', 'Elementen', 'Schakelaars en beveiligingen'] #, 'Hoofdkabels']
+    loading = 'Belastinggraad'
+    voltages_phase = ['UL1', 'UL2', 'UL3']
+    voltages_phase_n = ['UL1N', 'UL2N', 'UL3N']
+    sort = 'Soort'
+    trafo = 'transformator'
+    cable = 'kabel'
+else:
+    ValueError('Language not recognized')
+
+include = voltages_phase + voltages_phase_n + [loading, sort]
 
 voltage_max = 253
 voltage_min = 207
 
-voltage_results = {}
-loading_results = {}
-voltage_violations = {}
-loading_violations = {}
-for elem in sheets:
-    voltage_results[elem] = {}
-    loading_results[elem] = []
-    voltage_violations[elem] = 0
-    loading_violations[elem] = 0
-loading_results[trafo] = []
-loading_violations[trafo] = 0
+cwd = os.getcwd()
+all_files = os.listdir(cwd)
+excel_files = [filename for filename in all_files if filename.endswith('fuses.xlsx')]
 
+# define scenarios
+scenarios = {}
+nr = 1
+for filename in excel_files:
+    scenarios[nr] = {'id': nr, 'file': filename,
+                     'voltage_violations': {}, 'loading_violations': {}, 'fuse_violations': {},
+                     'total_v_violations': 0, 'total_l_violations': 0, 'total_f_violations': 0,
+                     }
+    nr += 1
 
-# --- data analysis ---
-df = {}
-df_v_results = {}
-# Load files
-for elem in sheets:
-    # import files as dataframes per sheet
-    df[elem] = pd.read_excel(filename, engine=engine, sheet_name=elem, header=0, skiprows=[1])
-    # only include relevant columns
-    df[elem] = df[elem][[name for name in include if name in df[elem].columns]]
-    # drop columns that contain NaN values because they are not valuable
-    df[elem] = df[elem].dropna()
+# start loop per file (scenario)
+for scenario in scenarios.keys():
+    entry = scenarios[scenario]
+    file = entry['file']
 
-    # check voltages
-    if any(voltages in df[elem].columns.to_list() for voltages in voltages):
-        # check per phase voltage:
-        for voltage in [col for col in voltages if col in df[elem].columns]:
-            voltage_results[elem][voltage] = []
+    # pre-allocation
+    voltage_violations = {}
+    loading_violations = {trafo: 0}
+    fuse_violations = {}
+
+    df = {}
+    # Analysis per Excel sheet
+    for elem in sheets:
+        # import files as dataframes per sheet
+        df[elem] = pd.read_excel(file, engine=engine, sheet_name=elem, header=0, skiprows=[1])
+        # only include relevant columns
+        df[elem] = df[elem][[name for name in include if name in df[elem].columns]]
+        # drop rows that contain NaN values because they are not useful
+        df[elem] = df[elem].dropna()
+        # initialize counting
+        voltage_violations[elem] = 0
+        loading_violations[elem] = 0
+        fuse_violations[elem] = 0
+
+        # check voltages at nodes
+        if elem == sheets[0]:
             for i in df[elem].index:
-                if df[elem][voltage][i] > voltage_max or df[elem][voltage][i] < voltage_min:
-                    voltage_results[elem][voltage].append(1)
-                else:
-                    voltage_results[elem][voltage].append(0)
-        # aggregate phases to sum violation at # of nodes:
-        df_v_results[elem] = pd.DataFrame(voltage_results[elem])
-        for i in df_v_results[elem].index:
-            # print(df_v_results[elem].iloc[i])
-            if df_v_results[elem].iloc[i].sum() > 0:
-                voltage_violations[elem] += 1
-    # check loadings
-    if loading in df[elem].columns:
-        for i in df[elem].index:
-            if sort in df[elem].columns:
-                if df[elem].at[i, sort] == cable:
-                    if df[elem][loading][i] > 100:
-                        loading_results[elem].append(1)
-                    else:
-                        loading_results[elem].append(0)
-                elif df[elem][sort].loc[i] == trafo:
-                    if df[elem][loading][i] > 100:
-                        loading_results[trafo].append(1)
-                    else:
-                        loading_results[trafo].append(0)
-            else:
-                if df[elem][loading][i] > 100:
-                    loading_results[elem].append(1)
-                else:
-                    loading_results[elem].append(0)
-    # sum number of violations
-    loading_violations[elem] = sum(loading_results[elem])
-    loading_violations[trafo] = sum(loading_results[trafo])
+                if all(x > 0 for x in df[elem][voltages_phase].loc[i].to_list()):
+                    if any(x < 207.0 for x in df[elem][voltages_phase].loc[i].to_list()):
+                        voltage_violations[elem] += 1
+                    # if single phase ?
 
-total_voltage_violations = sum(voltage_violations.values())
-total_loading_violations = sum(loading_violations.values())
+        # check loading of cables and transformers
+        if elem == sheets[1]:
+            for i in df[elem].index:
+                if df[elem].at[i, sort] == trafo:
+                    if df[elem][loading].loc[i] > 100:
+                        loading_violations[trafo] += 1
+                else:
+                    if df[elem][loading].loc[i] > 100:
+                        loading_violations[elem] += 1
 
-# print results
-print('voltage violations: ', voltage_violations)
-print('loading violations: ', loading_violations)
-print('total voltage violations: ', total_voltage_violations)
-print('total loading violations: ', total_loading_violations)
+        # check voltages and loadings at connections (elements)
+        if elem == sheets[2]:
+            for i in df[elem].index:
+                if all(x > 0 for x in df[elem][voltages_phase_n].loc[i].to_list()):
+                    if any(x < 207.0 for x in df[elem][voltages_phase_n].loc[i].to_list()):
+                        voltage_violations[elem] += 1
+                    # if single phase ?
+                if df[elem][loading].loc[i] > 100:
+                    loading_violations[elem] += 1
+
+        # check fuses
+        if elem == sheets[3]:
+            for i in df[elem].index:
+                if df[elem][loading].loc[i] > 100:
+                    fuse_violations[elem] += 1
+
+    entry['voltage_violations'] = voltage_violations
+    entry['loading_violations'] = loading_violations
+    entry['fuse_violations'] = fuse_violations
+    entry['total_v_violations'] = sum(voltage_violations.values())
+    entry['total_l_violations'] = sum(loading_violations.values())
+    entry['total_f_violations'] = sum(fuse_violations.values())
+
+    # print results
+    print('Scenario: ', entry['id'])
+    print('voltage violations: ', voltage_violations)
+    print('loading violations: ', loading_violations)
+    print('fuse violations: ', fuse_violations)
+    print('total voltage violations: ', entry['total_v_violations'])
+    print('total loading violations: ', entry['total_l_violations'])
+    print('total fuse violations: ', entry['total_f_violations'])
+
+end = time.time()
+time = end-start
+print('time: ', round(time, 4))
 
 # --- Graphics ---
+
+
