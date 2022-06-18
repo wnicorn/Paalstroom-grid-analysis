dc753c69a19c38386e946f25151e275e95d8d09b
Elise <e.v.wijngaarden@student.tue.nl>
2022-06-18 16:47:39 +0200

    prep scripts done

---
+++ create_result_folders.py
@@ -0,0 +1,27 @@
+"""
+Author: Elise van Wijngaarden
+Updated: 17 June 2022
+
+This script creates the folders that are needed in order to save the Gaia simulation results in the correct places.
+
+INSTRUCTIONS:
+only change the variables under 'Input':
+- Edit 'results_dir' to the folder where all result folders should be saved (where Gaia will later save excel results).
+- Edit 'data_dir' to the location where all the Gaia network files are located (on which the analyses will be performed)
+"""
+
+# ------------------- Input: -------------------------
+results_dir = 'C:/Users/Elise/Desktop/Gaia/'  # where folders with excel files are saves from the Gaia results
+data_dir = 'G:/.shortcut-targets-by-id/19-JqZkBCPZFbYYHLjE-rEmM8zRKqqge7/5LEF0 - SIP - Paalstroom/Research/2 Tech' \
+           'nical Evaluation/Grid/Collected Data/Enexis/Round 2/'
+# ---------------------------------------------------
+
+
+import os
+
+list_files = os.listdir(data_dir)
+gnf_file_names = [filename[-14:-4] for filename in list_files if filename.endswith('.gnf')]
+
+for dir_name in gnf_file_names:
+    path = os.path.join(results_dir, dir_name)
+    os.makedirs(path, exist_ok=True)
--- network_types.py
+++ network_types.py
@@ -1,22 +1,37 @@
 import pandas as pd
-import os
+import math
+import numpy as np
 
 
-# filename = 'G:/.shortcut-targets-by-id/19-JqZkBCPZFbYYHLjE-rEmM8zRKqqge7/5LEF0 - SIP - Paalstroom/Research/2 Technical Evaluation/Grid/Collected Data/Enexis/Round 2/20220307_Results_SL_R2102-02_gG-Light.xlsx'
-# df = pd.read_excel(filename, sheet_name='Steekproef', engine='openpyxl', header=0)
-# df = df[['KABELGROEP', 'NETSTATION', 'NETSTATION_STEDELIJKHEID', 'NETWERKTYPE', 'KABELGROEP_AANLEGJAAR', 'KABELGROEP_DECENIUM',
-#          'KABELGROEP_LENGTE', 'NODE_MAX_FOUTSPANNING', 'N_OV_AANSLUITING']]
-# print(df.head())
-#
-# df.count()
-# df.KABELGROEP.unique()
+filename = 'G:/.shortcut-targets-by-id/19-JqZkBCPZFbYYHLjE-rEmM8zRKqqge7/5LEF0 - SIP - Paalstroom/Research/2 Tech'\
+           'nical Evaluation/Grid/Collected Data/Enexis/Round 2/20220307_Results_SL_R2102-02_gG-Light.xlsx'
+df = pd.read_excel(filename, sheet_name='Steekproef', engine='openpyxl', header=0)
+df = df[['KABELGROEP', 'NETSTATION_VESTIGING', 'NETSTATION', 'NETSTATION_STEDELIJKHEID', 'NETWERKTYPE', 'KABELGROEP_AANLEGJAAR',
+         'KABELGROEP_DECENIUM', 'KABELGROEP_LENGTE', 'NODE_MAX_FOUTSPANNING', 'N_OV_AANSLUITING']]
 
-results_dir = 'C:/Users/Elise/Desktop/Gaia/'  # where folders with excel files are saves from the Gaia results
-data_dir = 'G:/.shortcut-targets-by-id/19-JqZkBCPZFbYYHLjE-rEmM8zRKqqge7/5LEF0 - SIP - Paalstroom/Research/2 Technical Evaluation/Grid/Collected Data/Enexis/Round 2/'
+df['KABELGROEP_LENGTE_TYPE'] = df['KABELGROEP_LENGTE']
+df['FOUTSPANNING_TYPE'] = df['NODE_MAX_FOUTSPANNING']
+df['N_OV_AFGEROND'] = df['N_OV_AANSLUITING']
 
-list_files = os.listdir(data_dir)
-gnf_file_names = [filename[-14:-4] for filename in list_files if filename.endswith('.gnf')]
+for i in df.index:
+    df['KABELGROEP_LENGTE_TYPE'].loc[i] = math.ceil(df['KABELGROEP_LENGTE'].loc[i].astype(float) / 100) * 100
+    df['FOUTSPANNING_TYPE'].loc[i] = math.floor(df['NODE_MAX_FOUTSPANNING'].loc[i].astype(float) / 50) * 50
+    df['N_OV_AFGEROND'].loc[i] = math.floor(df['N_OV_AANSLUITING'].loc[i].astype(float) / 5) * 5
 
-for dir_name in gnf_file_names:
-    path = os.path.join(results_dir, dir_name)
-    os.makedirs(path)
+print(df.head())
+
+counts = {'total': len(df),
+          'kabelgroep': len(df.KABELGROEP.unique()),
+          'station': len(df.NETSTATION.unique()),
+          'stedelijkheid': np.unique(df.NETSTATION_STEDELIJKHEID, return_counts=True),
+          'Vestiging': np.unique(df.NETSTATION_VESTIGING, return_counts=True),
+          'type': np.unique(df.NETWERKTYPE.dropna(inplace=False), return_counts=True),
+          'aanleg_decenium': np.unique(df.KABELGROEP_DECENIUM, return_counts=True),
+          'rond_lengte': np.unique(df.KABELGROEP_LENGTE_TYPE, return_counts=True),
+          'foutspanning_type': np.unique(df.FOUTSPANNING_TYPE, return_counts=True),
+          'n_ov': np.unique(df.N_OV_AFGEROND, return_counts=True)
+          }
+for entry in counts.keys():
+    print(entry, ':')
+    print(counts[entry], '\n')
+# print(counts)
